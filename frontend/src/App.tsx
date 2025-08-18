54100862dc31d7dcebff76a67de8ec52f1dbd36f
leandroatochkin <leandro.atochkin@outlook.es>
2025-08-18 18:29:40 -0300

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,24 @@
-
-import Home from './views/home/Home'
-import NotFound from './views/notFound/NotFound'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-
+import Home from './views/home/Home';
+import NotFound from './views/notFound/NotFound';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
-
   const router = createBrowserRouter([
-  {
-    path: "/:itemId",
-    element: (
-      <Home />
-    ),
-    errorElement: <NotFound />,
-  }
-])
+    {
+      path: "/:itemId",
+      element: <Home />,
+    },
+    {
+      path: "/",
+      element: <Home />, // optional: default home
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    }
+  ]);
 
-  return (
-    <>
-        <RouterProvider router={router} />
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
